5bee42e85fef1dd57670adf784bb584600942f70
mimi <you@example.com>
2025-05-21 18:14:50 +0200

    norminette init.c

--- src/parser/init.c
+++ src/parser/init.c
@@ -6,7 +6,7 @@
 /*   By: mdoudi-b <mdoudi-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:30:47 by fsaffiri          #+#    #+#             */
-/*   Updated: 2025/05/21 17:45:23 by mdoudi-b         ###   ########.fr       */
+/*   Updated: 2025/05/21 18:14:33 by mdoudi-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,25 @@ static int	clean_tokens(t_msh *msh)
 {
 	if (!msh->tokens || msh->parse_error)
 		return (0);
-	
 	expand_flag(msh->tokens);
 	expand_tokens(&msh->tokens, msh);
-	
 	return (1);
 }
 
-static int is_only_whitespace(const char *str) {
-	int i = 0;
+static int	is_only_whitespace(const char *str)
+{
+	int	i;
+
+	i = 0;
 	if (!str)
-		return 1;
-	while (str[i]) {
+		return (1);
+	while (str[i])
+	{
 		if (str[i] != ' ' && str[i] != '\t')
-			return 0;
+			return (0);
 		i++;
 	}
-	return 1;
+	return (1);
 }
 
 void	init_msh(char **envp, t_msh *msh)
@@ -42,8 +44,8 @@ void	init_msh(char **envp, t_msh *msh)
 	msh->cmd_len = 0;
 	msh->parse_error = 0;
 	msh->state = 0;
-	msh->exit_requested = 0;  // Inicializar a falso
-	msh->exit_code = 0;       // Inicializar a 0
+	msh->exit_requested = 0; // Inicializar a falso
+	msh->exit_code = 0;      // Inicializar a 0
 	msh->cmd = NULL;
 	msh->tokens = NULL;
 	msh->path = NULL;
@@ -68,7 +70,6 @@ void	get_input(t_msh *msh)
 				if (msh->cmd)
 				{
 					executor(msh);
-					
 					// Verifica si se ha solicitado salir
 					if (msh->exit_requested)
 					{
@@ -78,7 +79,6 @@ void	get_input(t_msh *msh)
 				}
 			}
 		}
-		
 		// Limpia estructuras de comandos y tokens antes de leer la siguiente entrada
 		if (msh->cmd)
 		{
@@ -89,7 +89,6 @@ void	get_input(t_msh *msh)
 		{
 			free_tokens(&msh->tokens);
 		}
-		
 		msh->input = readline("Mimishell% ");
 		if (!msh->input)
 			ctrl_d();
